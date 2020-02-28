6e87125c8edfae7107888ed7ec2dfe7ede6debc2
captus85 <gyu_han@hotmail.com>
2020-02-28 11:20:23 +0900

    modify2

--- added_variable_plot.R
+++ added_variable_plot.R
@@ -21,7 +21,7 @@ ad.lm <- ad.lm %>%
 
 ad.lm$DM[ad.lm$DM == 0] <- 2 
 ad.lm$DM <- factor(ad.lm$DM)
-
+ad.lm$DM <- relevel(ad.lm$DM, ref = "2")
 
 
 ad.dm <- ad.lm %>%
@@ -60,10 +60,10 @@ ggplot(ad.nondm, aes(x = Homocysteine, y = SPARE_AD_index)) +
 
 
 
-fit1 <- lm(SPARE_BA_index ~ dia + sex + age + edu + VRSnonDM + VitB12 + 
+fit1 <- lm(SPARE_AD_index ~ dia + sex + age + edu + VRSnonDM + VitB12 + 
              Folate + Creatinine + Homocysteine, data = ad.nondm)
 
-fit2 <- lm(SPARE_BA_index ~ dia + sex + age + edu + VRSnonDM + VitB12 + 
+fit2 <- lm(SPARE_AD_index ~ dia + sex + age + edu + VRSnonDM + VitB12 + 
              Folate + Creatinine + Homocysteine, data = ad.dm)
 
 (mod_vars = all.vars( formula(fit1) )[-1])
@@ -106,31 +106,59 @@ preds_dm = pred_dats2 %>%
               upper = .fitted + 2*.se.fit,
               pred = .fitted))
 
-
+#fit1, fit2에서 SPARE_AD_index, SPARE_BA_index로 수정하면서 아래 plot 그리기
 
 ggplot(ad.lm, aes(x = Homocysteine, y = SPARE_AD_index, col = DM)) + 
   geom_point(alpha = 0.6) +
   scale_color_manual(name = "DM",
-                     values = c("#fac409", "#0b8def"),
-                     labels=c("Positive", "Negative"))  +
-  geom_line(data = preds_dm$Homocysteine, aes(y = pred), size = 1, col = "#fac409") +
-  geom_line(data = preds_nondm$Homocysteine, aes(y = pred), size = 1, col = "#0b8def") +
+                     values = c("#2F66F7", "#a50000"),
+                     labels=c("Negative", "Positive"))  +
+  geom_line(data = preds_dm$Homocysteine, aes(y = pred), size = 1, col = "#a50000") +
+  geom_line(data = preds_nondm$Homocysteine, aes(y = pred), size = 1, col = "#2F66F7") +
   xlab("Serum Homocysteine") +
   ylab("SPARE-AD index") +
   scale_x_continuous(expand=c(0.01,0.01)) +
-  theme(plot.margin = unit(c(1,1,1,1), "cm")) +
-  theme_classic()
+  theme(
+    legend.position = c(0.9, 0.9),
+    legend.title = element_text(size = 15),
+    legend.text = element_text(size = 12),
+    axis.title.x = element_text(size = 20,  face="bold",
+                                margin = margin(t = 20, r = 0, b = 0, l = 0)),
+    axis.text.x = element_text(size = 20,  face="bold"),
+    axis.title.y = element_text(size = 20, face="bold",
+                                margin = margin(t = 0, r = 20, b = 0, l = 0)),
+    axis.text.y = element_text(size = 20, face="bold"),
+    plot.margin = unit(c(1,1,1,1), "cm"),
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(colour = "black"),
+    panel.border = element_blank(),
+    panel.background = element_blank())
 
 
 ggplot(ad.lm, aes(x = Homocysteine, y = SPARE_BA_index, col = DM)) + 
   geom_point(alpha = 0.6) +
   scale_color_manual(name = "DM",
-                     values = c("#fac409", "#0b8def"),
-                     labels=c("Positive", "Negative"))  +
-  geom_line(data = preds_dm$Homocysteine, aes(y = pred), size = 1, col = "#fac409") +
-  geom_line(data = preds_nondm$Homocysteine, aes(y = pred), size = 1, col = "#0b8def") +
+                     values = c("#2F66F7", "#a50000"),
+                     labels=c("Negative", "Positive"))  +
+  geom_line(data = preds_dm$Homocysteine, aes(y = pred), size = 1, col = "#a50000") +
+  geom_line(data = preds_nondm$Homocysteine, aes(y = pred), size = 1, col = "#2F66F7") +
   xlab("Serum Homocysteine") +
   ylab("SPARE-BA index") +
   scale_x_continuous(expand=c(0.01,0.01)) +
-  theme(plot.margin = unit(c(1,1,1,1), "cm")) +
-  theme_classic()
+  theme(
+    legend.position = c(0.9, 0.9),
+    legend.title = element_text(size = 15),
+    legend.text = element_text(size = 12),
+    axis.title.x = element_text(size = 20,  face="bold",
+                                margin = margin(t = 20, r = 0, b = 0, l = 0)),
+    axis.text.x = element_text(size = 20,  face="bold"),
+    axis.title.y = element_text(size = 20, face="bold",
+                                margin = margin(t = 0, r = 20, b = 0, l = 0)),
+    axis.text.y = element_text(size = 20, face="bold"),
+    plot.margin = unit(c(1,1,1,1), "cm"),
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(colour = "black"),
+    panel.border = element_blank(),
+    panel.background = element_blank())
